9c98db286cb942c70edc84f9886db9d4944fcbc8
maulana <maulana@localhost>
2016-06-07 14:27:31 +0700

    add user model

---
+++ application/core/MY_Model.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of MY_model
+ *
+ * @author maulana
+ */
+class MY_Model extends CI_Model{
+    protected $name = '';
+    protected $db = NULL;
+    
+
+
+    public function __construct() {
+        parent::__construct();
+        $CI = &get_instance();
+        $this->db = $CI->load->database('default', TRUE);
+    }
+            
+
+    public function insert($data) {
+
+         $this->db->insert($this->name, $data);
+    }
+    
+    public function update($id, $data) {
+        $this->db->where($this->id, $id);
+        $this->db->update($this->name, $data); 
+    }
+    
+    public function all() {
+        return $this->db->get($this->name)->result_array();
+    }
+    
+    
+    public function delete($id) {
+        $this->db->delete($this->name, array($this->id => $id)); 
+    }
+    
+    
+    
+    
+    
+    public function isIdExists($id) {
+        $query = $this->db->get_where($this->name, array($this->id => $id))->result_array();
+        return count($query)? TRUE: FALSE;
+    }
+    
+    
+    
+}
---
+++ application/models/User_model.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Description of User_model
+ *
+ * @author maulana
+ */
+
+class User_model extends MY_Model {
+    protected $name = 'users_pawon';
+    protected $id =  'uuid';
+    public function setData($data) {
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
